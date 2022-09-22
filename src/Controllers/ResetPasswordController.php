6eb2483676dd049816e4540d0c9f660b3b52698e
Anthnoybs <anthonybs.pro@gmail.com>
2022-09-22 15:59:44 +0200

    font awsome

--- src/Controllers/ResetPasswordController.php
+++ src/Controllers/ResetPasswordController.php
@@ -47,11 +47,12 @@ class ResetPasswordController extends BaseController
         self::init();
         $alert = false;
 
-        if (empty($_GET['confirm__key'])) 
+        if (empty($_POST['confirm__key'])) 
             $alert = ['message' => 'Le lien est defectueux'];
 
-        if (empty($_GET['confirm__user'])) 
-            $alert = ['message' => 'Le lien est defectueux'];
+        if ($_POST['user__password'] != $_POST['confirm__pass'])
+            $alert = ['message' => 'Les mots de passe doivent etre identiques'];
+        
 
 
         return self::$twig->render(
@@ -62,6 +63,19 @@ class ResetPasswordController extends BaseController
             );
         
     }
+
+
+    // public static function post(){
+    //     $config = file_get_contents('config.json');
+    //     $config = json_decode($config);
+    //     $Security = new AuthService();
+    //     $Client = new GuzzleClient();
+    //     if (empty($_POST['confirm__key'])) {
+    //         # code...
+    //     }
+    //     self::init();
+    //     $alert = false;
+    // }
 
 
     public static function updatePasssword($key , $user__mail , $user__password , GuzzleClient $GuzzleClient ) {
@@ -79,15 +93,17 @@ class ResetPasswordController extends BaseController
     public  static function getLink(GuzzleClient $GuzzleClient , string $email){
         
         try {
-            $response = $GuzzleClient->get('/RESTapi/forgot/password' , [
+            $response = $GuzzleClient->get('/RESTapi/forgot' , [
                 'query' => [ 'user__mail' =>  $email]
             ]);
             
         } catch (ClientException $exeption) {
             $response = $exeption->getResponse();
+            
         }
         $message = json_decode($response->getBody()->read(1024));
-       
+   
+      
         if (intval($response->getStatusCode()) != 200) 
             return false;
 
